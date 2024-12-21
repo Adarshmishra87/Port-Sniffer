b046ca6f1c04a1fca8ba7b0db5ddf2a19ab06e8d
Adarsh Mishra <143173802+Adarshmishra87@users.noreply.github.com>
2024-12-21 23:07:44 +0530

    Update Port_Scanner.py

--- Port_Scanner.py
+++ Port_Scanner.py
@@ -1,7 +1,6 @@
 import socket
 from datetime import datetime
 
-# Function to scan a port on a target host
 def scan_port(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(1)  # Timeout for connection attempts
@@ -11,15 +10,11 @@ def scan_port(host, port):
     else:
         print(f"Port {port} is closed.")
     sock.close()
-
-# Main function to scan a range of ports on a target host
 def scan_ports(target):
     print(f"Scanning ports on {target}...")
-    # Scan ports from 20 to 1024 (commonly used ports)
     for port in range(20, 1025):
         scan_port(target, port)
 
 if __name__ == "__main__":
-    # Target IP address (change to your target IP for testing)
     target_ip = "192.168.1.1"
     scan_ports(target_ip)
